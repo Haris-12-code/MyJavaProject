7b78f5ddda92a212cd73eb26d4ff1775abc73cf1
Muhammad Haris <mhariscom139@gmail.com>
2025-10-27 18:58:14 +0500

    Initial commit

---
+++ InfixEvaluation1.java
@@ -0,0 +1,59 @@
+import java.util.Stack;
+public class InfixEvaluation1 {
+    public static void main(String[] args) {
+        String expression = "3 + 5 * (2 - 8)";
+        System.out.println("Result: " + evaluateInfix(expression));
+    }
+    public static int evaluateInfix(String expression) {
+        Stack<Integer> values = new Stack<>();
+        Stack<Character> operators = new Stack<>();
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+            if (c == ' ') {
+                continue;
+            }
+            if (Character.isDigit(c)) {
+                int num = 0;
+                while (i < expression.length() && Character.isDigit(expression.charAt(i))) {
+                    num = num * 10 + (expression.charAt(i) - '0');
+                    i++;
+                }
+                values.push(num);
+                i--; 
+            } else if (c == '(') {
+                operators.push(c);
+            } else if (c == ')') {
+                while (operators.peek() != '(') {
+                    values.push(applyOperator(operators.pop(), values.pop(), values.pop()));
+                }
+                operators.pop(); 
+            } else if (isOperator(c)) {
+                while (!operators.isEmpty() && precedence(operators.peek()) >= precedence(c)) {
+                    values.push(applyOperator(operators.pop(), values.pop(), values.pop()));
+                }
+                operators.push(c);
+            }
+        }
+        while (!operators.isEmpty()) {
+            values.push(applyOperator(operators.pop(), values.pop(), values.pop()));
+        }
+        return values.pop();
+    }
+    public static boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/';
+    }
+    public static int precedence(char operator) {
+        if (operator == '+' || operator == '-') return 1;
+        if (operator == '*' || operator == '/') return 2;
+        return 0;
+    }
+    public static int applyOperator(char operator, int b, int a) {
+        switch (operator) {
+            case '+': return a + b;
+            case '-': return a - b;
+            case '*': return a * b;
+            case '/': return a / b;
+        }
+        return 0;
+    }
+}
